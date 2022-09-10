5524e615b35269fe498b3cec962cd732a0a309ee
OlegPustovalov <106656451+OlegPustovalov@users.noreply.github.com>
2022-09-10 17:12:58 +0300

    добавление кода версия от 10 09 2022

---
+++ people.py
@@ -0,0 +1,6 @@
+def get_employees():
+    print('Cотрудников нет, никого нет!')
+    return
+
+if __name__ == '__main__':
+    print ('Готовы к работам 5 разработчиков')
---
+++ salary.py
@@ -0,0 +1,6 @@
+def calculate_salary():
+    print('Денег  нет,ни копеечки!')
+    return
+
+if __name__ == '__main__':
+    print('Бюджет проекта 1000000 рублей')
